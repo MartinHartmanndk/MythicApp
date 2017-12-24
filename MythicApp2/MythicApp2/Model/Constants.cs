5c9a6f27a88626abd67388dfe9d03c0867414ca5
MHN <MHN@DESKTOP-GETSFEU>
2017-12-24 13:07:23 +0100

    Upload project

---
+++ MythicApp2/MythicApp2/Model/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MythicApp2.Model
+{
+    class Constants
+    {
+        public static bool IsDev = true;
+    }
+}
---
+++ MythicApp2/MythicApp2/Model/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MythicApp2.Model
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public User() { }
+
+        public User(string UserName, string Password)
+        {
+            this.UserName = UserName;
+            this.Password = Password;
+        }
+    }
+}
